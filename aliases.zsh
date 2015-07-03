30c4c826240986e4db004368764b8178369bcc5e
Brian Zelip <bzelip@gmail.com>
2015-07-03 13:02:22 -0400

    auto updating config files

--- aliases.zsh
+++ aliases.zsh
@@ -10,6 +10,7 @@ alias refresh='source ~/.zshrc'
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles TRUE'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles FALSE'
 alias upDots='cp ~/.zshrc ~/.dotfiles && cp ~/.oh-my-zsh/custom/aliases.zsh ~/.dotfiles && cp ~/.oh-my-zsh/custom/prompt.zsh ~/.dotfiles/'
+alias updateDots='cp ~/.zshrc ~/.dotfiles && cp ~/.oh-my-zsh/custom/aliases.zsh ~/.dotfiles && cp ~/.oh-my-zsh/custom/prompt.zsh ~/.dotfiles && git add ~/.dotfiles/* && git commit ~/.dotfiles/ -m "auto updating config files" && git push'
 
 # Applications
 alias ffdev='open -a FirefoxDeveloperEdition'
