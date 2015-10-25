281651e42b518fc6702da2c729b8b699963da472
Brian Zelip <bzelip@gmail.com>
2015-10-25 10:04:16 -0400

    auto updating config files

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -24,6 +24,7 @@ alias weka='open -a weka-3-6-12-oracle-jvm'
 alias rstudio='open -a RStudio'
 alias airport='open -a AirPort\ Utility'
 alias torrent='open -a Transmission'
+alias xld='open -a XLD'
 
 
 # Files
